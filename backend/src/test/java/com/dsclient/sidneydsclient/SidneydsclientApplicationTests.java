9d40b053ef577658e1f9c1df0f8795cf32eba500
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-03-12 20:42:28 -0300

    Project created

---
+++ backend/src/test/java/com/dsclient/sidneydsclient/SidneydsclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsclient.sidneydsclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidneydsclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
